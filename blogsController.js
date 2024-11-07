0535a1e927858741ccf6daf11828fc3c62952689
shahriyar2426 <shahriyaralam2761@gmail.com>
2024-11-07 23:02:37 +0600

    first commit

---
+++ blogsController.js
@@ -0,0 +1,11 @@
+
+router.post("/create-blog", blogController.createBlog);  
+
+
+router.get("/read-blog", blogController.readBlog);
+
+
+router.put("/update-blog", blogController.updateBlog);
+
+
+router.delete("/delete-blog", blogController.deleteBlog);
